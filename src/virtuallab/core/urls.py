ad6458808c1d7385e172ab56789a15b98130e755
Sizov <ser-reb@yandex.ru>
2024-03-28 09:40:41 +0400

    добавление страницы регистрации и входа

--- src/virtuallab/core/urls.py
+++ src/virtuallab/core/urls.py
@@ -5,10 +5,15 @@ URL configuration for virtuallab project.
 from django.contrib import admin
 from django.urls import path
 
-from main import views
+from main import views as mviews
+from users import views as uviews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
-    path('', views.main, name='main'),
+    path('', mviews.main, name='main'),
+    
+    path('register', uviews.register_teacher, name='teacher_register'),
+    path('login', uviews.login, name='user_login'),
+    path('logout', uviews.logout, name='user_logout'),
 ]
--- src/virtuallab/users/views.py
+++ src/virtuallab/users/views.py
@@ -1,3 +1,58 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+from django.contrib.auth import (
+    authenticate, 
+    login as auth_login,
+    logout as auth_logout,
+)
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+
+def register_teacher(request):
+    if request.method == 'GET':
+        form = UserCreationForm()
+    
+    elif request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('main', permanent=True)
+    
+    return render(
+        request,
+        'register_teacher.html',
+        {'form': form}
+    )
+
+    
+def login(request):
+    if request.method == 'GET':
+        form = AuthenticationForm()
+    
+    elif request.method == 'POST':
+        
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            auth_login(request, form.get_user())
+            return redirect('main', permanent=True)
+        
+        # user = authenticate(
+        #     request,
+        #     username=request.POST['username'],
+        #     password=request.POST['password'],
+        # )
+        # auth_login(request, user)
+        # return redirect('main', permanent=True)
+    
+    
+    return render(
+        request,
+        'login.html',
+        {'form': form}
+    )
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('main', permanent=True)
