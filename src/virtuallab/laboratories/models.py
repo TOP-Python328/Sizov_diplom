cc34ce6880f0331262aa533b9dd2f21510f34561
Sizov <ser-reb@yandex.ru>
2024-03-29 17:05:06 +0400

    правки моделей

--- src/virtuallab/laboratories/models.py
+++ src/virtuallab/laboratories/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import Teacher, Schoolboy
+
 
 class Laboratory(models.Model):
     class Meta:
@@ -27,9 +29,9 @@ class TaskSolution(models.Model):
     class Meta:
         db_table = 'tasks_solution'
     
-    time_start = DateTimeField()
-    time_end = DateTimeField()
+    time_start = models.DateTimeField(auto_now_add=True)
+    time_end = models.DateTimeField()
     solution = models.CharField(max_length=200)
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
-#    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-#    users = models.ForeignKey(Schoolboys, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
+    users = models.ForeignKey(Schoolboy, on_delete=models.CASCADE)
