7ece2822c3283b1f24ff6460122f4e0e07645807
Sizov <ser-reb@yandex.ru>
2024-04-01 20:38:55 +0400

    правки моделей пользователей после консультации

--- src/virtuallab/users/models.py
+++ src/virtuallab/users/models.py
@@ -5,33 +5,31 @@ from django.contrib.auth.models import User
 
 class Person(models.Model):
     class Meta:
-        db_table = 'persons'
+#        db_table = 'persons'
+        abstract = True
         
-    person = models.OneToOneField(
+    user = models.OneToOneField(
         User,
         on_delete=models.CASCADE,
         primary_key=True,
     )     
-    last_name = models.CharField(max_length=30)
-    first_name = models.CharField(max_length=30)
-    patr_name = models.CharField(max_length=30)
+    last_name = models.CharField(verbose_name='Фамилия', max_length=30)
+    first_name = models.CharField(verbose_name='Имя', max_length=30)
+    patr_name = models.CharField(verbose_name='Отчество', max_length=30)
+    uid = models.CharField(max_length=10, unique=True)
+    organization = models.CharField(max_length=50)
 
  
-class Teacher(models.Model):
+class Teacher(Person):
     class Meta:
         db_table = 'teachers'
         
-    person = models.OneToOneField(
-        Person,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
-    post = models.CharField(max_length=100)
+    post = models.CharField(verbose_name='Должность', max_length=100)
 
 
-class Grayd(models.Model):
+class Group(Person):
     class Meta:
-        db_table = 'grayds'
+        db_table = 'groups'
     
     title = models.CharField(max_length=10) 
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
@@ -39,16 +37,10 @@ class Grayd(models.Model):
       
 class Schoolboy(models.Model):
     class Meta:
-        db_table = 'schoolboys'
-        
-#    person = models.OneToOne(Person) 
-    person = models.OneToOneField(
-        Person,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
+        db_table = 'schoolboys'        
+
     teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE)
-    grayd = models.ForeignKey(Grayd, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
     
 
 
